f7e65aadb3d313f628128c42f8af268c1f274756
kentislove <newlife691027@gmail.com>
2025-05-14 12:19:57 +0800

    Add files via upload

---
+++ channel/web_channel.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+import os
+from bridge.context import Context, ContextType
+from bridge.reply import Reply, ReplyType
+from channel.chat_channel import ChatChannel
+from common.log import logger
+
+class WebChannel(ChatChannel):
+    def __init__(self):
+        super().__init__()
+        self.app = Flask(__name__)
+        self.port = int(os.environ.get("PORT", 10000))
+        self.app.add_url_rule('/chat', 'chat', self.chat_handler, methods=['POST'])
+
+    def chat_handler(self):
+        data = request.json
+        context = Context(ContextType.TEXT, content=data.get("message", ""))
+        reply = self.produce(context)
+        return jsonify({"reply": reply.content})
+
+    def startup(self):
+        logger.info(f"Starting web service on 0.0.0.0:{self.port}")
+        self.app.run(host="0.0.0.0", port=self.port)
+
+def create_channel():
+    return WebChannel()
