fca169918291f203bddaf8d3bb705652efab49f2
kentislove <newlife691027@gmail.com>
2025-05-14 23:03:28 +0800

    Update singleton.py

--- common/singleton.py
+++ common/singleton.py
@@ -1,9 +1,9 @@
+# common/singleton.py
+
 def singleton(cls):
     instances = {}
-
     def get_instance(*args, **kwargs):
         if cls not in instances:
             instances[cls] = cls(*args, **kwargs)
         return instances[cls]
-
     return get_instance
