2e5aada30edb27907efb3c3b5d34e8e751ed19d7
kentislove <newlife691027@gmail.com>
2025-05-14 14:45:45 +0800

    Update web_channel.py

--- channel/web_channel.py
+++ channel/web_channel.py
@@ -10,7 +10,7 @@ from common.log import logger
 class WebChannel(ChatChannel):
     def __init__(self):
         super().__init__()
-        self.app = Flask(__name__, template_folder='templates')
+        self.app = Flask(__name__, template_folder='channel/web/templates')
         self.port = int(os.environ.get("PORT", 10000))
         self.app.add_url_rule('/chat', 'chat', self.chat_handler, methods=['POST'])
         self.app.add_url_rule('/', 'index', self.index_handler)
