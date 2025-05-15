5881fe8aa801ee8182722027b4509e933a4432f7
kentislove <newlife691027@gmail.com>
2025-05-15 09:12:27 +0800

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -2,16 +2,15 @@
 Auto-replay chat robot abstract class
 """
 
-
 from bridge.context import Context
 from bridge.reply import Reply
-
 
 class Bot(object):
     def reply(self, query, context: Context = None) -> Reply:
         """
         bot auto-reply content
-        :param req: received message
-        :return: reply content
+        :param query: received message
+        :param context: extra context info
+        :return: reply content (Reply object)
         """
-        raise NotImplementedError
+        raise NotImplementedError("子類別必須實作 reply 方法")
